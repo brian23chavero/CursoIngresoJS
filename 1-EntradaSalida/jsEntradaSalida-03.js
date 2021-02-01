dd1424feed0673dab3ad0181e2b2d5a7bf9f96d4
brian chavero <brianjuanc2@gmail.com>
2021-02-01 08:48:16 -0300

    ejercicios hechos

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -3,10 +3,17 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	//reservo un lugar en la memoria para guardar el nombre del usuario 
+	let nombre;
 
-}
+	//
+	nombre = document.getElementById("txtIdNombre").value;
+
+    //muestra el mensaje de promp
+	alert(nombre);
+
+	document.getElementById("txtIdNombre").value = ""
+	}
+
 
 
--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,15 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	// reservo un lugar en la memoria para guardar el nombre del usuario
+
+	let nombre;
+
+	// guardo en la variable nombre el texto que escribio el usuario en la ventana promp 
+	nombre = prompt ("escriba su nombre");
+
+	//copio el nombre que tengo guadado en la variable nombre dentro de la caja de texto embebida en la pagina html 
+	document.getElementById("txtIdNombre").value = nombre
+
 }
 
